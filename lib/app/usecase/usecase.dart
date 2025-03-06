3b589362e36bd00ea9002ad84c89794052cc8176
sulav97 <sulavkatuwal2@gmail.com>
2025-03-06 11:44:15 +0545

    made few changes

---
+++ lib/app/usecase/usecase.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+import '../../core/error/failure.dart';
+
+abstract interface class UsecaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+abstract interface class UsecaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
